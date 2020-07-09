52b11676a81cb944dc3e3eaf90ddc6e732b2200c
Jason Long <jlong@github.com>
2020-07-08 21:31:36 -0400

    Oops, don't disable zoom

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function IndexPage() {
       <Head>
         <title>Jason Long</title>
         <meta name="description" content="Personal site of Jason Long"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="twitter:description" content="GitHub Product Designerd" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="white" />
