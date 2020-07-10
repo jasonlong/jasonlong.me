0412c330254f491bceab6fe789d00b44533a3eb4
Jason Long <jlong@github.com>
2020-07-10 16:16:58 -0400

    Add designerd definition

--- components/hero.js
+++ components/hero.js
@@ -4,9 +4,13 @@ export default function Hero() {
       <h1 className="font-heading tracking-tight text-4xl sm:text-5xl md:text-6xl font-bold leading-tight">
         Hi. I'm Jason.
       </h1>
-      <h2 className="text-xl sm:text-2xl md:text-3xl text-gray-600">
-        I'm a product designer at GitHub.
+      <h2 className="sm:text-xl md:text-2xl text-gray-700">
+        de·sign·erd <span className="text-gray-600">| \ di-ˈzī-nərd \</span>
       </h2>
+      <p className="text-sm md:text-base text-gray-600">
+        <span className="italic block">noun</span>
+        <span className="ml-4 block">a person who enjoys creating things both visually and with code.</span>
+      </p>
     </div>
   )
 }
