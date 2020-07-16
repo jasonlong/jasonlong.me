50c4757f97d959c9ee983a9a3b28bd8076885323
Jason Long <jlong@github.com>
2020-07-16 09:09:43 -0400

    Try to speed up google font loading
    https://medium.com/clio-calliope/making-google-fonts-faster-aadf3c02a36d

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function IndexPage() {
         <title>Jason Long</title>
         <meta name="description" content="Personal site of Jason Long"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@800&display=swap" rel="stylesheet" />
         <script async defer data-domain="jasonlong.me" src="https://plausible.io/js/plausible.js"></script>
       </Head>
