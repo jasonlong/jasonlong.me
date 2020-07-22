7d043d70178c913ad3c1603a9e3d8fe27ca74727
Jason Long <jlong@github.com>
2020-07-22 12:08:20 -0400

    Final logo shape working

--- components/logo.js
+++ components/logo.js
@@ -1,14 +1,68 @@
 import { Canvas, useFrame } from 'react-three-fiber'
-import { Suspense, useRef } from "react"
+import { Suspense, useRef, useMemo } from "react"
+import * as THREE from 'three'
 
 function Model() {
   const SIZE = 10
 
+  const jMatArray = useMemo(() => [
+    new THREE.MeshBasicMaterial({color:0xD53F8C}),
+    new THREE.MeshBasicMaterial({color:0xD53F8C}),
+    new THREE.MeshBasicMaterial({color:0xF687B3}),
+    new THREE.MeshBasicMaterial({color:0xF687B3}),
+    new THREE.MeshBasicMaterial({color:0xD53F8C}),
+    new THREE.MeshBasicMaterial({color:0xD53F8C})
+  ])
+
+  const midMatArray = useMemo(() => [
+    new THREE.MeshBasicMaterial({color:0x667EEA}),
+    new THREE.MeshBasicMaterial({color:0x667EEA}),
+    new THREE.MeshBasicMaterial({color:0xB794F4}),
+    new THREE.MeshBasicMaterial({color:0xB794F4}),
+    new THREE.MeshBasicMaterial({color:0xD53F8C}),
+    new THREE.MeshBasicMaterial({color:0xD53F8C})
+  ])
+
+  const lMatArray = useMemo(() => [
+    new THREE.MeshBasicMaterial({color:0x667EEA}),
+    new THREE.MeshBasicMaterial({color:0x667EEA}),
+    new THREE.MeshBasicMaterial({color:0x7F9CF5}),
+    new THREE.MeshBasicMaterial({color:0x7F9CF5}),
+    new THREE.MeshBasicMaterial({color:0xD53F8C}),
+    new THREE.MeshBasicMaterial({color:0xD53F8C})
+  ])
+
   return (
     <object3D>
-      <mesh position={[0, 0, 0]}>
+      <mesh position={[-SIZE * 2, -SIZE, 0]} material={jMatArray}>
         <boxBufferGeometry attach="geometry" args={[SIZE, SIZE, SIZE]} />
-        <meshBasicMaterial attach="material" color="#D53F8C" />
+      </mesh>
+      <mesh position={[-SIZE * 2, 0, 0]} material={jMatArray}>
+        <boxBufferGeometry attach="geometry" args={[SIZE, SIZE, SIZE]} />
+      </mesh>
+
+      <mesh position={[-SIZE , -SIZE, 0]} material={jMatArray}>
+        <boxBufferGeometry attach="geometry" args={[SIZE, SIZE, SIZE]} />
+      </mesh>
+
+      <mesh position={[0, -SIZE, 0]} material={midMatArray}>
+        <boxBufferGeometry attach="geometry" args={[SIZE, SIZE, SIZE]} />
+      </mesh>
+      <mesh position={[0, 0, 0]} material={midMatArray}>
+        <boxBufferGeometry attach="geometry" args={[SIZE, SIZE, SIZE]} />
+      </mesh>
+      <mesh position={[0, SIZE, 0]} material={midMatArray}>
+        <boxBufferGeometry attach="geometry" args={[SIZE, SIZE, SIZE]} />
+      </mesh>
+      <mesh position={[0, SIZE * 2, 0]} material={midMatArray}>
+        <boxBufferGeometry attach="geometry" args={[SIZE, SIZE, SIZE]} />
+      </mesh>
+
+      <mesh position={[0, -SIZE, -SIZE]} material={lMatArray}>
+        <boxBufferGeometry attach="geometry" args={[SIZE, SIZE, SIZE]} />
+      </mesh>
+      <mesh position={[0, -SIZE, -SIZE * 2]} material={lMatArray}>
+        <boxBufferGeometry attach="geometry" args={[SIZE, SIZE, SIZE]} />
       </mesh>
     </object3D>
   )
