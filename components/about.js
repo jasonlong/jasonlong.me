1b5a7b509db79e70b7f7bc76b58a5a1fba9e87ef
Jason Long <jlong@github.com>
2020-07-26 21:43:48 -0400

    Add about section

---
+++ components/about.js
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable global-require */
+/* eslint-disable react/jsx-one-expression-per-line */
+
+export default function Projects() {
+  return (
+    <>
+      <picture>
+        <source
+          srcSet={require('../public/images/profile.jpg?webp')}
+          type="image/webp"
+        />
+        <source
+          srcSet={require('../public/images/profile.jpg?')}
+          type="image/jpeg"
+        />
+        <img
+          src={require('../public/images/profile.jpg')}
+          className="rounded-full w-16 md:w-20 mt-12 mx-auto"
+          alt="Jason Long"
+        />
+      </picture>
+
+      <h2 className="text-center pb-4 px-8 mt-4 text-large md:text-xl font-black dark:text-gray-100">
+        I&apos;m a product designer.
+      </h2>
+
+      <p className="text-center text-base md:text-xl text-gray-700 dark:text-gray-500 mx-auto px-8 md:px-20 mb-16">
+        I live in Columbus, Ohio and work for{' '}
+        <a href="https://github.com">GitHub</a>. I&apos;m current working on{' '}
+        <a href="https://github.com/features/actions">GitHub Actions</a>, but
+        I&apos;ve had the opportunity to work on a wide variety of projects
+        since joining in 2012.
+      </p>
+    </>
+  )
+}
