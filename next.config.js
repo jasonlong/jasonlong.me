88c6409b883f16e9a525f6a7539c88b0371e03ae
Jason Long <jlong@github.com>
2020-07-28 11:41:07 -0400

    Clean up unused plugins

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
+const withPlugins = require('next-compose-plugins')
+const optimizedImages = require('next-optimized-images')
 
 module.exports = withPlugins([
-  [optimizedImages, {
-    optimizeImagesInDev: true,
-    mozjpeg: {
-      quality: 85
-    },
-    optipng: {},
-    webp: {},
-    responsiveLoader: {},
-    imageTrace: {
-      color: 'rgba(0, 0, 0, 0.2)'
+  [
+    optimizedImages,
+    {
+      optimizeImagesInDev: true,
+      mozjpeg: {
+        quality: 85
+      },
+      optipng: {},
+      webp: {},
+      responsiveLoader: {}
     }
-  }]
-]);
+  ]
+])
