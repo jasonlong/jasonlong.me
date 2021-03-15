8b26782bb9ae8d64b62ed7d39105ad1cc2efc519
Jason Long <jason@jasonlong.me>
2021-03-15 11:50:00 -0400

    Update bio

--- components/about.js
+++ components/about.js
@@ -3,20 +3,19 @@ import Image from 'next/image'
 export default function Projects() {
   return (
     <>
-      <div className="w-16 md:w-20 mt-12 mx-auto">
+      <div className="w-16 mx-auto mt-12 md:w-20">
         <Image src="/images/profile.jpg" alt="Jason Long" width={192} height={192} className="rounded-full" />
       </div>
 
-      <h2 className="text-center pb-4 px-8 mt-4 text-large md:text-xl font-black dark:text-gray-100">
+      <h2 className="px-8 pb-4 mt-4 font-black text-center text-large md:text-xl dark:text-gray-100">
         I&apos;m a product designer.
       </h2>
 
-      <p className="text-center text-base md:text-xl text-gray-700 dark:text-gray-500 mx-auto px-8 md:px-20 mb-16">
-        I live in Columbus, Ohio and work for{' '}
-        <a href="https://github.com">GitHub</a>. I&apos;m current working on{' '}
-        <a href="https://github.com/features/actions">GitHub Actions</a>, but
-        I&apos;ve had the opportunity to work on a wide variety of projects
-        since joining in 2012.
+      <p className="px-8 mx-auto mb-16 text-base text-center text-gray-700 md:text-xl dark:text-gray-500 md:px-20">
+        I live in Columbus, Ohio and I&apos;m helping design next-generation database tools at{' '}
+        <a href="https://planetscale.com">PlanetScale</a> . I was previously at{' '}
+        <a href="https://github.com">GitHub</a> where I worked on{' '}
+        <a href="https://github.com/features/actions">Actions</a>.
       </p>
     </>
   )
