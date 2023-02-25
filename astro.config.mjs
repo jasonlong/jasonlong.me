049d7ec549cd87fd2f772c72ae6110bf1c8eef58
Jason Long <jason@jasonlong.me>
2023-02-25 17:16:26 -0500

    Add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: netlify()
 });
