ec58db1742c92f89aad307ed8089e09d820b3d91
Jason Long <jason@jasonlong.me>
2023-02-25 17:13:40 -0500

    Remove deno stuff again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-import deno from "@astrojs/deno";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
-  adapter: deno()
+  integrations: [tailwind()]
 });
