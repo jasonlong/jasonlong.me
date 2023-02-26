1bbf227882e44bbfd531705cab3cb6aa5ad3671e
Jason Long <jason@jasonlong.me>
2023-02-26 17:16:52 -0500

    Add github star count

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   output: "server",
   adapter: netlify()
 });
