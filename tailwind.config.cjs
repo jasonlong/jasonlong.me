71253c89ebd86aa961503fe3ae46820dd82207c4
Jason Long <jason@jasonlong.me>
2023-02-26 10:23:59 -0500

    Use zinc and sky colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,14 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-    colors: {
-      'black': '#000',
-      'yellow': '#ffc833',
-      'yellow-accent': '#c39724',
-      'yellow-accent-dark': '#845e10',
-      'gray-900': '#211f1a'
-    }
+    // colors: {
+    //   'black': '#000',
+    //   'yellow': '#ffc833',
+    //   'yellow-accent': '#c39724',
+    //   'yellow-accent-dark': '#845e10',
+    //   'gray-900': '#211f1a'
+    // }
 	},
-	plugins: [],
 }
