462628153504ca6b06dcf21354859ac5fa753dc2
Jason Long <jason@jasonlong.me>
2023-03-04 20:10:14 -0500

    Remove unused colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-    // colors: {
-    //   'black': '#000',
-    //   'yellow': '#ffc833',
-    //   'yellow-accent': '#c39724',
-    //   'yellow-accent-dark': '#845e10',
-    //   'gray-900': '#211f1a'
-    // }
-	},
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 }
