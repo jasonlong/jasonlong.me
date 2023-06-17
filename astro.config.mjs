fb6c11af5be4d0750c7ea99a0ea02c64f41ac809
Jason Long <jason@jasonlong.me>
2023-06-17 12:28:53 -0400

    Clean up comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/functions";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import netlify from '@astrojs/netlify/functions'
+import mdx from '@astrojs/mdx'
 
-// https://astro.build/config
-import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  output: "server",
-  adapter: netlify()
-});
+  output: 'server',
+  adapter: netlify(),
+})
